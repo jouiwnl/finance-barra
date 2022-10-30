e225c75050319efec249366b40d5cc9a867951f9
jouiwnl <jou.098olo@gmail.com>
2022-10-30 19:53:15 -0300

    fix permissions on routes

--- src/main/java/com/finance/barra/controller/UsuarioController.java
+++ src/main/java/com/finance/barra/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import com.finance.barra.model.Usuario;
 import com.finance.barra.service.UserSS;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,6 +30,7 @@ public class UsuarioController {
 
     @PostMapping
     @Transactional(propagation = Propagation.REQUIRED)
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public UsuarioDto create(@RequestBody Usuario usuario) {
         usuario.setSenha(encoder.encode(usuario.getSenha()));
         return UsuarioDto.of(this.repository.save(usuario));
@@ -36,6 +38,7 @@ public class UsuarioController {
 
     @PutMapping("{id}")
     @Transactional(propagation = Propagation.REQUIRED)
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public UsuarioDto update(@PathVariable("id") Long id, @RequestBody Usuario usuario) {
         Usuario finded = this.repository.findOne(Usuario.class, QUsuario.usuario.id.eq(id));
         usuario.setSenha(finded.getSenha());
@@ -44,6 +47,7 @@ public class UsuarioController {
 
     @DeleteMapping("{id}")
     @Transactional(propagation = Propagation.REQUIRED)
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public ResponseEntity<Object> delete(@PathVariable("id") Long id) {
         Usuario finded = this.repository.findOne(Usuario.class, QUsuario.usuario.id.eq(id));
         this.repository.remove(finded);
@@ -53,6 +57,7 @@ public class UsuarioController {
 
     @GetMapping
     @Transactional(readOnly = true)
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public List<UsuarioDto> findAll() {
         return this.repository.findAll(Usuario.class)
                 .stream()
@@ -63,6 +68,7 @@ public class UsuarioController {
 
     @GetMapping("{id}")
     @Transactional(readOnly = true)
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public UsuarioDto findOne(@PathVariable("id") Long id) {
         return UsuarioDto.of(this.repository.findOne(Usuario.class, QUsuario.usuario.id.eq(id)));
     }
